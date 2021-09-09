a9a15d22a58b1901131234a109158ce6acecee28
Radevirot <78053632+Radevirot@users.noreply.github.com>
2021-09-09 16:58:31 -0300

    Corrección de numeración de ejercicios

--- Unidad 2/PROGRAMACION - Ejercicio 10.cpp
+++ Unidad 2/PROGRAMACION - Ejercicio 10.cpp
@@ -1,50 +1,52 @@
 #include <iostream>
+#include <string>
 #include <list>
-#include <queue>
 using namespace std;
 
 list<int> generar_lista(int tam){
 	list<int> lista;
 	for(int i=0; i<tam; i++){
-		lista.push_back(rand()%10+1);
+		lista.push_back(rand()%100+1);
 	}
 	return lista;
 }
 	
-void mostrar_lista(list<int> &L){
+void mostrar_lista(list<string> &L){
 	cout << "[";
-	for( list<int>::iterator it=L.begin(); it!=--L.end(); ++it ) {cout << *it << ",";}
+	for( list<string>::iterator it=L.begin(); it!=--L.end(); ++it ) {cout << *it << ",";}
 	cout << *(--L.end()) << "]" << endl;
 }
 	
-void compacta(list<int> &L,list<int> &S){
-	list<int>::iterator SitB=S.begin(),SitE=S.end(),LitB=L.begin();
-	int suma=0;
-	while(SitB!=SitE){
-		if((*SitB)>0){
-			for(int i=0;i<*SitB;i++) { 
-				suma+=(*LitB);
-				LitB = L.erase(LitB);
-				if (LitB==L.end()) break;
-			}
-			L.insert(LitB,suma);
-			suma=0;
-			if (LitB==L.end()) return;
+list<string> josephus(list<string> &nombres,int n){
+	int randomSoldier=0, contador=0;
+	list<string> sortedNames;
+	list<string>::iterator itB=nombres.begin();
+	advance(itB,randomSoldier-1);
+	while(!nombres.empty()){
+		cout << contador << endl;
+		if(contador==n){
+			sortedNames.push_back(*itB);
+			itB = nombres.erase(itB);
+			contador=0;
+			itB--;
+		}
+		if(++itB==nombres.end()){
+			itB=nombres.begin();
 		}
 		
-		++SitB;
+		
+		++contador;
 	}
+	return sortedNames;
 }
-	
 
 int main(int argc, char *argv[]) {
 	
-	list<int> L ={1,7,8,2,3}, S ={2,2,2,2,2};
-	mostrar_lista(L); mostrar_lista(S);
+	list<string> L{"A","B","C","D","E","F","G","H"};
+	mostrar_lista(L);
+	list<string> nueva=josephus(L,3);
+	mostrar_lista(nueva);
 	
-	compacta(L,S);
-	
-	mostrar_lista(L); mostrar_lista(S);
 	
 	return 0;
 }
